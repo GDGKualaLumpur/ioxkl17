d148da1fd722af157c7a35c8cd29aaae9d1b9b35
limhenry <henrylim.everything@gmail.com>
2017-07-19 21:58:27 +0800

    update

--- functions/index.js
+++ functions/index.js
@@ -11,31 +11,85 @@ let options = {};
 
 exports.makeConfirmation = functions.https.onRequest((request, response) => {
     console.log(request.query.hash);
-console.log(request.query.confirmation);
-console.log(request.method);
+    console.log(request.query.confirmation);
+    console.log(request.method);
 
-readData()
-    .then(() =>  checkDeadline())
-.then(() => initApp())
-.then(_ => initSpreadsheet())
-.then(rows => updateSpreadsheet(rows, request.query.hash))
-.then(data => updateData(data.row, data.rows, data.updateRow, request.query.confirmation))
-    //todo
-.then(result => response.redirect(`${options.redirect_to}?msg=${result}`))
-.catch(reason => response.send("Something went wrong, your confirmation is not confirmed. Please contact JT via email: jt@gdg.my\nError: " + reason));
+    readData()
+        .then(() => checkDeadline())
+        .then(() => initApp())
+        .then(_ => initSpreadsheet())
+        .then(rows => updateSpreadsheet(rows, request.query.hash))
+        .then(data => updateData(data.row, data.rows, data.updateRow, request.query.confirmation))
+        //todo
+        .then(result => response.redirect(`${options.redirect_to}?msg=${result}`))
+        .catch(reason => response.send("Something went wrong, your confirmation is not confirmed. Please contact JT via email: jt@gdg.my\nError: " + reason));
+});
+
+exports.ticket = functions.https.onRequest((req, res) => {
+    msg = req.query.msg;
+    res.status(200).send(`<!doctype html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, minimum-scale=1, initial-scale=1, user-scalable=yes">
+        <title>Google I/O Extended 2017 Kuala Lumpur</title>
+        <link rel="icon" href="https://ioxkl17.firebaseapp.com/images/favicon.png">
+        <meta http-equiv="refresh" content="5;url=https://ioxkl17.firebaseapp.com/" />
+        <style>
+            body {
+                -webkit-box-pack: center;
+                    -ms-flex-pack: center;
+                        justify-content: center;
+                display: -webkit-box;
+                display: -ms-flexbox;
+                display: flex;
+                -webkit-box-orient: vertical;
+                -webkit-box-direction: normal;
+                    -ms-flex-direction: column;
+                        flex-direction: column;
+                min-height: 100vh;
+                text-align: center;
+                font-family: Roboto, sans-serif;
+                padding: 0 12px;
+            }
+            img {
+                margin: 24px auto;
+                max-width: 240px;
+            }
+            .big {
+                font-weight: 500;
+                font-size: 1.4em;
+                margin: 32px 0 4px 0;
+                color: #212121;
+            }
+            p {
+                color: #424242;
+                font-size: 0.95em;
+            }
+            a {
+                color: #2047fd;
+                font-weight: 500;
+            }
+        </style>
+    </head>
+    <body>
+        <img src="https://ioxkl17.firebaseapp.com/images/io.svg">
+        <p class="big">${msg}</p>
+        <p>You will be redirected automatically in 5 seconds, or click <a href="https://ioxkl17.firebaseapp.com/">here</a>.</p>
+    </body>
+  </html>`);
 });
 
 function readData() {
     console.log('readData');
     return new Promise((resolve, reject) => {
         admin.database().ref('/admin').once('value', (data) => {
-        options = data.val();
-    console.log(options);
-    resolve();
-}, (error) => {
-        reject(error.code);
-    })
-});
+            options = data.val();
+            console.log(options);
+            resolve();
+        }, (error) => {
+            reject(error.code);
+        })
+    });
 }
 
 function checkDeadline() {
@@ -43,10 +97,10 @@ function checkDeadline() {
     const deadline = moment(options.deadline);
     return new Promise((resolve, reject) => {
         if (deadline.diff(moment(), 'days') < 0) {
-        reject(options.deadline_text)
-    }
-    resolve();
-});
+            reject(options.deadline_text)
+        }
+        resolve();
+    });
 
 }
 
@@ -60,36 +114,36 @@ function initApp() {
             ['https://www.googleapis.com/auth/spreadsheets'],
             null
         );
-    jwtClient.authorize(function (err, tokens) {
-        if (err) {
-            reject(err);
-        }
-        auth = jwtClient;
-        resolve();
-    });
-})
+        jwtClient.authorize(function (err, tokens) {
+            if (err) {
+                reject(err);
+            }
+            auth = jwtClient;
+            resolve();
+        });
+    })
 }
 
 function initSpreadsheet() {
     console.log('initSpreadsheet');
     return new Promise((resolve, reject) => {
         let sheets = google.sheets('v4');
-    sheets.spreadsheets.values.get({
-        auth: auth,
-        spreadsheetId: options.spreadsheet_id,
-        range: options.spreadsheet_range,
-    }, function (err, response) {
-        if (err) {
-            reject('The API returned an error: ' + err);
-        }
-        let rows = response.values;
-        if (rows.length == 0) {
-            reject('No data found.');
-        } else {
-            resolve(rows)
-        }
+        sheets.spreadsheets.values.get({
+            auth: auth,
+            spreadsheetId: options.spreadsheet_id,
+            range: options.spreadsheet_range,
+        }, function (err, response) {
+            if (err) {
+                reject('The API returned an error: ' + err);
+            }
+            let rows = response.values;
+            if (rows.length == 0) {
+                reject('No data found.');
+            } else {
+                resolve(rows)
+            }
+        });
     });
-});
 }
 
 function updateSpreadsheet(rows, hash) {
@@ -97,47 +151,47 @@ function updateSpreadsheet(rows, hash) {
 
     return new Promise((resolve, reject) => {
         let email = getHeaderIndex(rows, "email");
-    for (let i = 1; i < rows.length; i++) {
-        let row = rows[i];
-        if (hashMD5(row[email].toLowerCase(), options.hash) === hash) {
-            resolve({row, rows, updateRow: i})
+        for (let i = 1; i < rows.length; i++) {
+            let row = rows[i];
+            if (hashMD5(row[email].toLowerCase(), options.hash) === hash) {
+                resolve({ row, rows, updateRow: i })
+            }
         }
-    }
-    reject("Record not found")
-});
+        reject("Record not found")
+    });
 
 }
 
-function updateData(row, rows, i, confirmation){
+function updateData(row, rows, i, confirmation) {
     console.log(`update ${row}`);
     let sheets = google.sheets('v4');
     let name = getHeaderIndex(rows, "name");
     let status = getHeaderIndex(rows, "status");
     return new Promise((resolve, reject) => {
         if (row[status] === "CONFIRMED") {
-        resolve(options.already_confirmation_text);
-    } else if (row[status] === "CANCELLED") {
-        resolve(options.already_cancellation_text);
-    }
-    let requests = [];
-    requests.push(getWriteCellRequest(i, status, confirmation));
-    let batchUpdateRequest = {requests: requests};
-    sheets.spreadsheets.batchUpdate({
-        spreadsheetId: options.spreadsheet_id,
-        auth: auth,
-        resource: batchUpdateRequest
-    }, function (err, response) {
-        if (err) {
-            reject(err);
+            resolve(options.already_confirmation_text);
+        } else if (row[status] === "CANCELLED") {
+            resolve(options.already_cancellation_text);
         }
-        if (confirmation == "cancel") {
-            resolve(options.cancellation_text)
-        } else {
-            resolve(options.confirmation_text)
-        }
+        let requests = [];
+        requests.push(getWriteCellRequest(i, status, confirmation));
+        let batchUpdateRequest = { requests: requests };
+        sheets.spreadsheets.batchUpdate({
+            spreadsheetId: options.spreadsheet_id,
+            auth: auth,
+            resource: batchUpdateRequest
+        }, function (err, response) {
+            if (err) {
+                reject(err);
+            }
+            if (confirmation == "cancel") {
+                resolve(options.cancellation_text)
+            } else {
+                resolve(options.confirmation_text)
+            }
 
-    });
-})
+        });
+    })
 }
 
 function getWriteCellRequest(row, column, confirmation) {
@@ -145,10 +199,10 @@ function getWriteCellRequest(row, column, confirmation) {
     let textToWrite, color;
     if (confirmation == "cancel") {
         textToWrite = "CANCELLED";
-        color = {red: 1}
+        color = { red: 1 }
     } else {
         textToWrite = "CONFIRMED";
-        color = {green: 1};
+        color = { green: 1 };
     }
     return {
         updateCells: {
