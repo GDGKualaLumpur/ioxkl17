6f1222b0b75b7ac905e43754d6f4b4b3034e921c
Henry Lim <henrylim.everything@gmail.com>
2017-09-13 02:59:43 +0800

    update route

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import webapp2
 
-class MainHandler(webapp2.RequestHandler):
-
+class SlashRedirect(webapp2.RequestHandler):
   def get(self, *args, **kwargs):
     self.redirect('//' + self.request.host + '/ioxkl17/')
 
+class Io17extended(webapp2.RequestHandler):
+  def get(self, *args, **kwargs):
+    path = self.request.path_qs.split('/')
+    if (path[1] != 'io17extended'):
+        self.redirect('https://www.gdgkl.org')
+    else:
+        self.redirect('//' + self.request.host + '/ioxkl17/' + '/'.join(path[2:]))
+
 app = webapp2.WSGIApplication([
-    ('/ioxkl17.*', MainHandler),
+    ('/ioxkl17.*', SlashRedirect),
+    ('/io17extended.*', Io17extended)
 ], debug=True)
